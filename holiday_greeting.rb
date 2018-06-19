623b2f0d3129f8f9158e15aa3bd9ce7cf3593dd7
jessejkwk <janderson@nyatoday.com>
2018-06-19 15:08:17 +0000

    automatically backed up by learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,5 @@
+def holiday(to_mail, from_mail, holiday_mail)
+  puts "Happy #{holiday_mail}, #{to_mail}! From #{from_mail}"
+end
+
+holiday("Beyonce", "Jay-Z", "Mother's day")
--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,9 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day(name_mom = "Beyonce")
+  puts "Happy Mother's Day, #{name_mom}"
+end
+
+mothers_day()
+
 
